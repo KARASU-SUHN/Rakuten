99e056c407dc48d349175855746413fc77c43cce
KARASU-SUHN <43245579+KARASU-SUHN@users.noreply.github.com>
2022-11-23 21:15:26 +0900

    Add files via upload

---
+++ wordcloud.py
@@ -0,0 +1,24 @@
+from wordcloud import WordCloud
+import re
+from PIL import Image
+import numpy as np
+
+# text = open('/Users/karasu/Downloads/top.txt').read()
+# wordcloud = WordCloud(
+#             font_path='/Library/Fonts/Arial Unicode.ttf', # フォントファイルの指定
+#             background_color="whitesmoke",
+#             colormap="tab10",
+#             width=400,
+#             height=200
+#             ).generate(text)
+# wordcloud.to_file("/Users/karasu/Downloads/1.png")
+
+text = open('/Users/karasu/Downloads/genre.txt').read()
+wordcloud = WordCloud(
+            font_path='/Library/Fonts/Arial Unicode.ttf', # フォントファイルの指定
+            background_color="whitesmoke",
+            colormap="tab10",
+            width=400,
+            height=200
+            ).generate(text)
+wordcloud.to_file("/Users/karasu/Downloads/genre.png")
